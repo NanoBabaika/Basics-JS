ca8acb09cb0b33631cfcc580f1f8ee175a2ae1e2
Alexander <117478092+NanoBabaika@users.noreply.github.com>
2023-11-10 21:56:24 +0300

    Add files via upload

--- tasks JS/task1.js
+++ tasks JS/task1.js
@@ -1,6 +1,20 @@
 
-let hi = 'Hello! How are you?';
+const string = 'Hello! How are you?';
 
-let vowels = 'eooaeyou';
+const vowels = ["e", "o", "e", "y", "u"];
 
-console.log(vowels);
+const getVovels = stringtoFiler => {
+    let extractVowels = "";
+
+    for (let i = 0 ; i < stringtoFiler.length; i++){
+        const currentLetter = stringtoFiler[i].toLowerCase();
+
+        if(vowels.includes(currentLetter)){
+            extractVowels += currentLetter;
+        }
+    }
+
+    return extractVowels;
+}
+
+console.log(getVovels(string));
